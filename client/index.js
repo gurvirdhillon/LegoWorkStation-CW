818e0c6ad385a383a12698b58a3813c7f6b2c951
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-27 21:36:00 +0000

    added stuff to the index html

---
+++ client/index.js
@@ -0,0 +1,15 @@
+// const inputRange = document.querySelector('#inputRange');
+// inputRange.addEventListener('oncharge', updateTextInput.this.value);
+
+// function updateTextInput(val) {
+//   document.querySelector('#textInput').value = val;
+//   if (val === undefined) {
+//     return 'foo value';
+//   }
+// }
+
+// function grabAllFunctions() {
+//   updateTextInput();
+// }
+
+// window.addEventListener('load', grabAllFunctions);
