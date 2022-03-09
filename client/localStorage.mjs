1b60c42fdec9b1cc1dfba3e4e21e61c6efd2ce7d
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-03-09 13:42:30 +0000

    sound works. Yay!

--- client/localStorage.mjs
+++ client/localStorage.mjs
@@ -2,12 +2,13 @@
 // for loop that checks all the ID's of the legoDivs
 // then stringifys all the children within that ID and keeps it in storage.
 
-
 export function addProductToCart(id) {
   const targetBasket = document.querySelector('.showItems');
   targetBasket.textContent = id;
 }
 
+// export the function that will do some storage (handle the click on the button)
+// inside createDivs.mjs import that function and add it as an eventListener when creating the buttons
 
 // attaching music to a button so when checkout is clicked music begins
 
@@ -24,6 +25,7 @@ function init() {
 
 window.addEventListener('load', init);
 
-
-// export the function that will do some storage (handle the click on the button)
-// inside createDivs.mjs import that function and add it as an eventListener when creating the buttons
+/* code extracted from:
+ Karahanidi, A., & Elton, D. (2021). Javascript Audio Play on click.
+ Retrieved from https://stackoverflow.com/questions/18826147/javascript-audio-play-on-click
+ edited according to own requirements */
