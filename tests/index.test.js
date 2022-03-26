fb4bd71ec9b91981c4a9baa9f59d8674ce828926
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-03-26 12:13:19 +0000

    added card input validation

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,8 @@ test('loops through the id of the bricks', () =>{
     expect(getBrick.id).toBe(getBrick.id);
 });
 
+
+
 // import { addProductToCart } from '../client/localStorage.mjs';
 // test('adds a brick to the cart', () =>{
 //     expect(addProductToCart.id).toBe(addProductToCart.id);
