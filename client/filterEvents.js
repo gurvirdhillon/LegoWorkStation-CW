ffc056aa34d346096fa38a8bf58241c130059f6e
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-03-31 20:20:03 +0100

    added categories

--- client/filterEvents.js
+++
@@ -1,9 +0,0 @@
-const checkMe = document.querySelector('.checkBox');
-checkMe.addEventListener('click', onlyOneBox);
-
-function onlyOneBox(checkbox) {
-  const checkBoxes = document.querySelector('.checkBox');
-  checkBoxes.forEach((item) => {
-    if (item !== checkbox) item.checked = false;
-  });
-}
