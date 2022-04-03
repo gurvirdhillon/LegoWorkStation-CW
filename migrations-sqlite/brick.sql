b25c075d41190948d776de44f5596707ea4a2b39
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-04-03 12:40:15 +0100

    added real data for brick sql

--- migrations-sqlite/brick.sql
+++ migrations-sqlite/brick.sql
@@ -8,7 +8,8 @@ CREATE TABLE Legos (ProductID SERIAL PRIMARY KEY, ProductName VARCHAR(30),
 Colour VARCHAR(30), ProductPrice INT, ProductImage VARCHAR(30), Quantity INT, typeOfLego VARCHAR(30));
 
 INSERT INTO Legos(ProductID, ProductName, Colour, ProductPrice, ProductImage, Quantity, typeOfLego) 
-VALUES (1, 'Red Brick', 'Red', 10, 'redbrick.jpg', 10, 'Brick'),
-(2, 'Yellow Brick', 'Yellow', 20, 'yellowbrick.jpg', 11, 'Brick'),
-(3, 'Black brick', 'Black', 20, 'blackbrick.jpg', 11, 'Brick'),
-(4, 'Blue brick', 'Blue', 20, 'bluebrick.jpg', 300, 'Brick');
+VALUES (brk1, "red 2 by 4 brick", "red", 0.30, "red2by4.jpg", 40, "brick"),
+(brk2, "blue 2 by 4 brick", "blue", 0.40, "blue2by4.jpg", 30, "brick"),
+(brk3, "green 2 by 4 brick", "green", 0.20, "green2by4.jpg", 300, "brick"),
+(brk4, "yellow 2 by 4 brick", "yellow", 0.50, "yellow2by4.jpg", 10, "brick"),
+(brk5, "Green 4 by 4 brick", "green", 0.60, "green4by4.jpg", 20, "brick");
