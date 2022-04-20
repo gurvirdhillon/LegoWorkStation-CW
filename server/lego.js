9f3bab6139e0b1298da8fe76a12858262a79eb48
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-04-20 14:21:10 +0100

    deleted redundant code which was commented

--- server/lego.js
+++ server/lego.js
@@ -33,26 +33,6 @@ export async function findBrick(id) {
   db.get('SELECT * FROM Legos WHERE id = ?', id);
 }
 
-// export async function addBrick(brick) {
-//   const db = await dbConn;
-//   // const dbConn = init();
-//   await db.run('INSERT INTO bricks (name, img, p, colour, quantity) VALUES (?, ?, ?, ?, ?)');
-//   return listBricks();
-// }
-
-// export async function editBricks(updateBricks) {
-//   const dbConn = init();
-//   const id = updateBricks.id;
-//   const name = updateBricks.name;
-
-//   const statement = await db.run('UPDATE Bricks SET brick = ? WHERE id = ?', [name, id]);
-
-//   if (statement.changes === 0) {
-//     throw new Error('No bricks found');
-//   }
-//   return findBrick(id);
-// }
-
 // Code extracted from Kocepky, J., Dennis, M., & Boakes, R. (2020).
 // staged-simple-message-board/stages at master · portsoc/staged-simple-message-board.
 // GitHub. Retrieved from https://github.com/portsoc/staged-simple-message-board/tree/master/stages.
