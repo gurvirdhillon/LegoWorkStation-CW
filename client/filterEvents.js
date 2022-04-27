56d2b8116a3aba210a70d7f5507caaee1ee2cdb5
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-04-27 16:25:23 +0100

    added filters

--- client/filterEvents.js
+++ client/filterEvents.js
@@ -1,22 +1,3 @@
-export const bricks = [
-  { id: 'brk1', name: 'Red 2 by 4', colour: 'red', x: 2, y: 4, img: 'img/red2by4.jpg', p: '£0.30', quantity: '40', type: 'brick' },
-  { id: 'brk2', name: 'Blue 2 by 4', colour: 'blue', x: 2, y: 4, img: 'img/blue2by4.jpg', p: '£0.40', quantity: '20', type: 'brick' },
-  { id: 'brk3', name: 'Yellow 2 by 4', colour: 'yellow', x: 2, y: 4, img: 'img/yellow2by4.jpg', p: '£0.20', quantity: '300', type: 'brick' },
-  { id: 'brk4', name: 'Green 2 by 4', colour: 'green', x: 2, y: 4, img: 'img/green2by4.jpg', p: '£0.20', quantity: '22', type: 'brick' },
-  { id: 'brk5', name: 'Black 2 by 4', colour: 'black', x: 2, y: 4, img: 'img/black2by4.jpg', p: '£0.20', quantity: '91', type: 'brick' },
-  { id: 'brk6', name: 'Green 2 by 4', colour: 'green', x: 4, y: 4, img: 'img/green2by4c.jpg', p: '£0.10', quantity: '51', type: 'brick' },
-  { id: 'brk7', name: 'Red 2 by 1', colour: 'red', x: 2, y: 2, img: 'img/red2by1.jpg', p: '£0.05', quantity: '29', type: 'brick' },
-  { id: 'brk8', name: 'Mixed Crate', colour: 'All', x: 1, y: 1, img: 'img/legoCrate.jpeg', p: '£40', quantity: '10', type: 'set' },
-  { id: 'brk9', name: 'Bonsai!', colour: 'All', x: 1, y: 1, img: 'img/bonsai.png', p: '£15', quantity: '4', type: 'set' },
-  { id: 'brk10', name: 'Lego master set', colour: 'All', x: 1, y: 1, img: 'img/house.png', p: '£20', quantity: '10', type: 'set' },
-  { id: 'brk11', name: 'The education bundle', colour: 'All', x: 1, y: 1, img: 'img/educationBundle.jpg', p: '£30', quantity: '40', type: 'set' },
-  { id: 'brk12', name: 'The ol\' saloon', colour: 'All', x: 1, y: 1, img: 'img/saloon.png', p: '£45', quantity: '5', type: 'set' },
-  { id: 'brk13', name: 'The plane!', colour: 'All', x: 1, y: 1, img: 'img/rocketPlane.jpg', p: '£15', quantity: '35', type: 'set' },
-  { id: 'brk14', name: 'The ultimate minecraft set', colour: 'All', x: 1, y: 1, img: 'img/treehouse.jpeg', p: '£25', quantity: '5', type: 'brick' },
-  { id: 'brk15', name: 'The victors colloseum', colour: 'All', x: 1, y: 1, img: 'img/colloseum.jpg', p: '£400', quantity: '2', type: 'brick' },
-  { id: 'brk16', name: 'The Avengers X Justice League Set', colour: 'All', x: 1, y: 1, img: 'img/heroLego.jpg', p: '£25', quantity: '20', type: 'brick' },
-];
-
 const grabSet = document.querySelector('#set');
 grabSet.addEventListener('click', handleSets);
 
@@ -24,7 +5,9 @@ const targetMain = document.querySelector('#main');
 
 // console.log(bricks.find(filterDivs));
 
-function handleSets() {
+async function handleSets() {
+  let bricks = await fetch('/api/bricks');
+  bricks = await bricks.json();
   targetMain.classList.add('activateSet');
   for (const brick of bricks) {
     console.log(bricks);
@@ -45,7 +28,9 @@ function handleSets() {
 const grabBrick = document.querySelector('#OverallBrk');
 grabBrick.addEventListener('click', handleByBrick);
 
-function handleByBrick() {
+async function handleByBrick() {
+  let bricks = await fetch('/api/bricks');
+  bricks = await bricks.json();
   targetMain.classList.add('activateBrick');
   targetMain.textContent = bricks.find(bricks => bricks.type === 'brick');
   const createImageElem = document.createElement('img');
@@ -63,7 +48,9 @@ function handleByBrick() {
 const handleElem = document.querySelector('#colourFilter');
 handleElem.addEventListener('click', handleColour);
 
-function handleColour() {
+async function handleColour() {
+  let bricks = await fetch('/api/bricks');
+  bricks = await bricks.json();
   const grabColour = document.querySelector('#colourFilter');
   targetMain.classList.add('activateColour');
   // targetMain.textContent = grabColour.value;
